8e9012bb5a890773c93a25d5a61fb1a3b3b5df5a
phakawatbsoft <164451140+phakawatbsoft@users.noreply.github.com>
2024-06-28 09:40:19 +0700

    Add event

--- app.js
+++ app.js
@@ -22,3 +22,22 @@ function colorize() {
     this.style.color = randomColor()
 }
 
+window.addEventListener('keydown',function(e){
+    switch(e.code){
+        case 'ArrowUp':
+            console.log('UP!')
+            break
+        case 'ArrowDown':
+            console.log('DOWN!')
+            break
+        case 'ArrowLeft':
+            console.log('LEFT!')
+            break
+        case 'ArrowRight':
+            console.log('RIGHT!')
+            break
+        default:
+            console.log('IGNORE!')
+    }
+})
+
